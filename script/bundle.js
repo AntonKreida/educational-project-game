4bba5290464a08875ac88081aafe9c626fb6949d
Kreida Anton <kreida.anton@yandex.ru>
2022-11-05 00:31:18 +0500

    Updates

---
+++ script/bundle.js
@@ -0,0 +1,152 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.html":
+/*!************************!*\
+  !*** ./src/index.html ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n// Module\nvar code = \"<!DOCTYPE html>\\r\\n<html lang=\\\"en\\\">\\r\\n  <head>\\r\\n    <meta charset=\\\"UTF-8\\\" />\\r\\n    <meta http-equiv=\\\"X-UA-Compatible\\\" content=\\\"IE=edge\\\" />\\r\\n    <meta name=\\\"viewport\\\" content=\\\"width=device-width, initial-scale=1.0\\\" />\\r\\n    <title>First game</title>\\r\\n  </head>\\r\\n  <body class=\\\"body\\\">\\r\\n    <div class=\\\"top center js-top\\\">\\r\\n      <!--Main-->\\r\\n      <main class=\\\"main\\\">\\r\\n        <div class=\\\"main__inner\\\">\\r\\n          <h1 class=\\\"main__title title\\\">Выбери сложность</h1>\\r\\n          <form\\r\\n            class=\\\"main__form form js-form\\\"\\r\\n            action=\\\"/\\\"\\r\\n            method=\\\"GET\\\"\\r\\n            novalidate\\r\\n          >\\r\\n            <div class=\\\"form__control\\\">\\r\\n              <input\\r\\n                class=\\\"form__input input js-input\\\"\\r\\n                type=\\\"radio\\\"\\r\\n                id=\\\"lavel-one\\\"\\r\\n                name=\\\"lavel\\\"\\r\\n                value=\\\"1\\\"\\r\\n                required\\r\\n              />\\r\\n              <label class=\\\"form__label label js-label\\\" for=\\\"lavel-one\\\"\\r\\n                >1</label\\r\\n              >\\r\\n              <input\\r\\n                class=\\\"form__input input js-input\\\"\\r\\n                type=\\\"radio\\\"\\r\\n                id=\\\"lavel-two\\\"\\r\\n                name=\\\"lavel\\\"\\r\\n                value=\\\"2\\\"\\r\\n                required\\r\\n              />\\r\\n              <label class=\\\"form__label label js-label\\\" for=\\\"lavel-two\\\"\\r\\n                >2</label\\r\\n              >\\r\\n\\r\\n              <input\\r\\n                class=\\\"form__input input js-input\\\"\\r\\n                type=\\\"radio\\\"\\r\\n                id=\\\"lavel-three\\\"\\r\\n                name=\\\"lavel\\\"\\r\\n                value=\\\"3\\\"\\r\\n                required\\r\\n              />\\r\\n              <label class=\\\"form__label label js-label\\\" for=\\\"lavel-three\\\"\\r\\n                >3</label\\r\\n              >\\r\\n            </div>\\r\\n            <button class=\\\"form__button button\\\" type=\\\"submit\\\">Старт</button>\\r\\n          </form>\\r\\n        </div>\\r\\n      </main>\\r\\n      <!--/Main-->\\r\\n    </div>\\r\\n  </body>\\r\\n</html>\\r\\n\";\n// Exports\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (code);\n\n//# sourceURL=webpack://first_game/./src/index.html?");
+
+/***/ }),
+
+/***/ "./src/style/main.scss":
+/*!*****************************!*\
+  !*** ./src/style/main.scss ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://first_game/./src/style/main.scss?");
+
+/***/ }),
+
+/***/ "./src/scripts/app/global/index.js":
+/*!*****************************************!*\
+  !*** ./src/scripts/app/global/index.js ***!
+  \*****************************************/
+/***/ (() => {
+
+eval("window.globalStateApp = {\r\n  lavel: \"\",\r\n  timeGame: \"\",\r\n  status: {\r\n    lavelGame: \"\",\r\n    state: \"\",\r\n    result: \"\",\r\n  },\r\n  generationCard: \"\",\r\n  selectedCard: \"\",\r\n  renderPlayField: container => {\r\n    container.innerHTML = \"\";\r\n  },\r\n};\r\n\n\n//# sourceURL=webpack://first_game/./src/scripts/app/global/index.js?");
+
+/***/ }),
+
+/***/ "./src/scripts/app/handlers/index.js":
+/*!*******************************************!*\
+  !*** ./src/scripts/app/handlers/index.js ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst handlers = {\r\n  onSubmitForm: event => {\r\n    event.preventDefault();\r\n\r\n    const { target } = event;\r\n    const inputs = target.querySelectorAll(\".js-input\");\r\n    const top = target.closest('.js-top');\r\n\r\n    if (!target.checkValidity()) {\r\n      return;\r\n    }\r\n\r\n    inputs.forEach(input => {\r\n      if (!input.checked) {\r\n        return;\r\n      };\r\n\r\n      window.globalStateApp.lavel = input.value;\r\n      window.globalStateApp.renderPlayField(top);\r\n    });\r\n  },\r\n\r\n  onClickLabel: event => {\r\n    const target = event.target;\r\n    const labels = document.querySelectorAll(\".js-label\");\r\n\r\n    if (!target.closest(\".js-label\")) {\r\n      return;\r\n    }\r\n\r\n    labels.forEach(label => {\r\n      label.classList.remove(\"active\");\r\n    });\r\n\r\n    target.classList.add(\"active\");\r\n  },\r\n};\r\n\r\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (handlers);\r\n\n\n//# sourceURL=webpack://first_game/./src/scripts/app/handlers/index.js?");
+
+/***/ }),
+
+/***/ "./src/scripts/app/init.js":
+/*!*********************************!*\
+  !*** ./src/scripts/app/init.js ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _global_index_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./global/index.js */ \"./src/scripts/app/global/index.js\");\n/* harmony import */ var _global_index_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_global_index_js__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _handlers_index_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./handlers/index.js */ \"./src/scripts/app/handlers/index.js\");\n\r\n\r\n\r\nwindow.addEventListener(\"DOMContentLoaded\", () => {\r\n  const form = document.querySelector(\".js-form\");\r\n\r\n  form.addEventListener(\"click\", _handlers_index_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"].onClickLabel);\r\n  form.addEventListener(\"submit\", _handlers_index_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"].onSubmitForm);\r\n});\r\n\n\n//# sourceURL=webpack://first_game/./src/scripts/app/init.js?");
+
+/***/ }),
+
+/***/ "./src/scripts/index.js":
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_html__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../index.html */ \"./src/index.html\");\n/* harmony import */ var _style_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../style/main.scss */ \"./src/style/main.scss\");\n/* harmony import */ var _app_init__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./app/init */ \"./src/scripts/app/init.js\");\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://first_game/./src/scripts/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/scripts/index.js");
+/******/ 	
+/******/ })()
+;
