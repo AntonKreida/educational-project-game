5e2cbfd8765a57c7ba108898c03c0ddf6f786afd
Kreida Anton <kreida.anton@yandex.ru>
2022-11-15 01:11:25 +0500

    add func flip-flop

--- src/scripts/app/class/card.js
+++ src/scripts/app/class/card.js
@@ -2,34 +2,67 @@ class Card {
   constructor(element, getListCard) {
     this.element = element;
     this.data = getListCard();
+    this.hasFlipCard = false;
+    this.firstCard = null;
+    this.secondCard = null;
 
     this.renderCard = this.renderCard.bind(this);
+    this.onFlipCard = this.onFlipCard.bind(this);
 
     this.renderCard(this.element);
-    this.element.addEventListener('click', (event) => {
-      const target = event.target.closest('.js-card');
+    this.element.addEventListener('click', this.onFlipCard);
+  }
 
-      if (!target.classList.contains('js-card')) {
-        return;
-      }
+  renderCard(field) {
+    field.innerHTML = '';
 
-      target.classList.add('active');
+    this.data.forEach((item) => {
+      field.innerHTML += Card.template(item);
     });
   }
 
-  renderCard(element) {
-    element.innerHTML = '';
+  onFlipCard(event) {
+    const target = event.target.closest('.js-card');
 
-    this.data.forEach((item) => {
-      element.innerHTML += Card.template(item);
-    });
+    if (!target.classList.contains('js-card')) {
+      return;
+    }
+
+    target.classList.add('flip');
+
+    // TODO: Тест логики условия проверки совпадения, который уйдет в controller-card связывая это с моделью
+    if (!this.hasFlipCard) {
+      this.hasFlipCard = true;
+      this.firstCard = target;
+      return;
+    }
+
+    this.secondCard = target;
+    this.hasFlipCard = false;
+
+    this.checkMatchCard();
+  }
+
+  checkMatchCard() {
+    if (this.firstCard.dataset.card === this.secondCard.dataset.card) {
+      return;
+    }
+
+    this.unFlipCard();
+  }
+
+  unFlipCard() {
+    setTimeout(() => {
+      this.firstCard.classList.remove('flip');
+      this.secondCard.classList.remove('flip');
+    }, 500);
   }
 }
 
 Card.template = (card) => {
   return `
-  <div class="card-wrapper" data-card="${card.suit}-${card.name}">
-    <div class="card js-card">
+  <div class="card-wrapper">
+    <div class="card js-card" data-card="${card.suit}-${card.name}">
      <div class="card-front">
        <img class="card__img" src="./images/bg_card_real.png" alt="">
      </div>
